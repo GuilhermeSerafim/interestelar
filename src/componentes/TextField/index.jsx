0e06894c8debbff06a2eaec6a63a55fa132f0247
GuilhermeSerafim <guilerstudies@gmail.com>
2024-01-22 14:31:33 -0300

    Aprendendo a concatenar props em uma template string

--- src/componentes/TextField/index.jsx
+++ src/componentes/TextField/index.jsx
@@ -1,12 +1,14 @@
 import './TextField.css';
 
 const TextField = (props) => {
+    const placeholderModificada = `${props.placeholder}Testeeeeee`
+
     return (
         <div className="campo-texto">
             {/* Imprimindo prop direto no html */}
             <label>{props.label}</label>
             {/* Imprimindo prop como propriedade */}
-            <input placeholder={props.placeholder}/>
+            <input placeholder={placeholderModificada}/>
         </div>
     )
 };
