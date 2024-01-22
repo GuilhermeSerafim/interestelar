b52d37a9e4239a470a3f4d21eb355251bcc4993a
GuilhermeSerafim <guilerstudies@gmail.com>
2024-01-22 10:51:13 -0300

    Criando primeiro componente e estilizando ele

---
+++ src/componentes/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX - Como o React trabalha com a parte visual
+    return (
+        <header className="banner">
+            <img src="./imagens/banner.png" alt="Banner do interestellar"/>
+        </header>
+    )
+}
+
+export default Banner;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Aqui ele pega o root (elemento global do DOM) e renderiza nosso componente React App
+const root = ReactDOM.createRoot(document.getElementById('root')); // Essa API é parte do React Concurrent Mode e é usada para criar uma raiz de renderização assíncrona.
 root.render(
   <React.StrictMode>
     <App />
