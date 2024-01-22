7f5ab5d57823995d008535da12f2a74d95e7c05d
GuilhermeSerafim <guilerstudies@gmail.com>
2024-01-22 11:41:23 -0300

    Entendo o 'core' do react e deletando o que não usaremos durante o projeto

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 // Aqui ele pega o root (elemento global do DOM) e renderiza nosso componente React App
 const root = ReactDOM.createRoot(document.getElementById('root')); // Essa API é parte do React Concurrent Mode e é usada para criar uma raiz de renderização assíncrona.
 root.render(
-  <React.StrictMode>
+  <React.StrictMode> {/** Prevenir erro, mensagens de erros mais amigaveis (exibido no console) */}
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
