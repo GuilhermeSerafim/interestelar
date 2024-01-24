108334f0e24094e9c192921ee2793f6c4eba06b1
GuilhermeSerafim <guilerstudies@gmail.com>
2024-01-24 14:03:42 -0300

    Associando o estado atual da escolha de time, ao seletor de time

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { Button } from '@mui/material';
 import Banner from './componentes/Banner';
 import FormAstro from './componentes/FormAstro';
 import "./styles.css"
+import { useState } from 'react';
 
 function App() {
-  let escolhaDoTime = "";
+  // let escolhaDoTime = "";
+  const [escolhaDoTime, setEscolhaDoTime] = useState("");
   return (
     <div className="App">
       {/* Componente React */}
@@ -15,12 +17,36 @@ function App() {
         Aqui terá a exibição dos times, com um botão de "escolher esse time" que leva um default value pro select de escolher times
       */}
       {/* Não está funcionando por enquanto | Criar um componente apenas para isso */}
-      <Button color='secondary' size='large' variant="outlined" onClick={() => escolhaDoTime = "Astro Studies"} >Desvende os segredos dos Cosmos</Button>
-      <Button color='secondary' size='large' variant="outlined" onClick={() => escolhaDoTime = "Stellar Explorers"} >Seja um explorador estelar</Button>
-      <Button color='secondary' size='large' variant="outlined" onClick={() => escolhaDoTime = "Galaxy Infrastructure"} >Molde galáxias e construa dysons</Button>
-      <Button color='secondary' size='large' variant="outlined" onClick={() => escolhaDoTime = "Interspatial Support"} >Seja um semeador dos cosmos</Button>
+      <Button
+        color='secondary'
+        size='large'
+        variant="outlined"
+        onClick={() => setEscolhaDoTime("Astro Studies")} >
+        Desvende os segredos dos Cosmos
+      </Button>
+      <Button
+        color='secondary'
+        size='large'
+        variant="outlined"
+        onClick={() => setEscolhaDoTime("Stellar Explorers")} >
+        Seja um explorador estelar
+      </Button>
+      <Button
+        color='secondary'
+        size='large'
+        variant="outlined"
+        onClick={() => setEscolhaDoTime("Galaxy Infrastructure")} >
+        Molde galáxias e construa dysons
+      </Button>
+      <Button
+        color='secondary'
+        size='large'
+        variant="outlined"
+        onClick={() => setEscolhaDoTime("Interspatial Support")} >
+        Seja um semeador dos cosmos
+      </Button>
       {/* Crie seus Astros */}
-      <FormAstro defaultValueSelect={escolhaDoTime}/>
+      <FormAstro defaultValueSelect={escolhaDoTime} />
     </div>
   );
 }
--- src/componentes/TextField/index.jsx
+++ src/componentes/TextField/index.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './TextField.css';
 import MUITextField from '@mui/material/TextField';
-const TextField = (props) => {
+    const TextField = (props) => {
 
+    // Desestruturação do objeto
+    const {error, required, label, multiline, lines} = props;
     // valor - valor atual
     // setValor - função cheia de mecanismo, no qual o react entende que deve renderizar o componente
-    const [valor, setValor] = useState("Valor padrão");
+    const [valor, setValor] = useState("Valor inicial");
 
     const aoDigitado = (e) => {
         console.log(valor);
@@ -19,15 +21,15 @@ const TextField = (props) => {
                 value={valor}
                 fullWidth
                 id="outlined-basic"
-                error={props.error}
-                required={props.required}
-                label={props.label}
+                error={error}
+                required={required}
+                label={label}
                 variant="outlined"
                 color='secondary'
                 margin='dense'
                 style={{backgroundColor:"#fff"}}
-                multiline={props.multiline}
-                rows={props.lines}
+                multiline={multiline}
+                rows={lines}
             />
         </div>
     )
